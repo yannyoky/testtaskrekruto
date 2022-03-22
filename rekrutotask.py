0ba4e52f2b826043aca9c618a22d5036bbbc8f68
yannyoky <84124589+yannyoky@users.noreply.github.com>
2022-03-22 22:05:03 +0700

    Update rekrutotask.py

--- rekrutotask.py
+++ rekrutotask.py
@@ -4,10 +4,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def query_strings():
-    name = request.args['name']
-    message = request.args['message']
-    return '''<h1>{}</h1><h1>{}</h1>'''.format(name, message)
-
+    try:
+        name = request.args['name']
+        message = request.args['message']
+        return '''<h1>{}</h1><h1>{}</h1>'''.format(name, message)
+    except:
+        name = 'Rekruto'
+        message = 'Давай дружить'
+        return '''<h1>{}</h1><h1>{}</h1>'''.format(name, message)
 #if __name__ == '__main__':
 #    app.run(debug=True)
 
