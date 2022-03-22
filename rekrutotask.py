381ab7c0c37bfb48e2fd948b589601bf15ad2d43
yannyoky <84124589+yannyoky@users.noreply.github.com>
2022-03-22 21:55:06 +0700

    Update rekrutotask.py

--- rekrutotask.py
+++ rekrutotask.py
@@ -7,7 +7,8 @@ def query_strings():
     name = request.args['name']
     message = request.args['message']
     return '''<h1>{}</h1><h1>{}</h1>'''.format(name, message)
-if __name__ == '__main__':
-    app.run(debug=True)
+
+#if __name__ == '__main__':
+#    app.run(debug=True)
 
 #http://127.0.0.1:5000/?name=Rekruto&message=Давай дружить!
