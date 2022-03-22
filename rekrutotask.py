195b58bdc9efc13ef9626959f72f563506f6d09d
yannyoky <84124589+yannyoky@users.noreply.github.com>
2022-03-22 22:26:29 +0700

    Update rekrutotask.py

--- rekrutotask.py
+++ rekrutotask.py
@@ -1,18 +1,11 @@
-from flask import Flask, request
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+
 
 @app.route('/', methods=['GET'])
-def query_strings():
+def main():
     try:
-        name = request.args['name']
-        message = request.args['message']
-        return '''<h1>{}</h1><h1>{}</h1>'''.format(name, message)
-    except:
-        name = 'Rekruto'
-        message = 'Давай дружить'
-        return '''<h1>{}</h1><h1>{}</h1>'''.format(name, message)
-#if __name__ == '__main__':
-#    app.run(debug=True)
-
-#http://127.0.0.1:5000/?name=Rekruto&message=Давай дружить!
+        return 'hello world'
+    except KeyError:
+        return f'Invalid input'
